bca1a5e08b17f174817e08afd655ec3ece0007f2
Sam Lachance <slachance@gmail.com>
2017-03-10 12:02:19 -0500

    increased wait_for_it timeout to 45 seconds

--- wp-entrypoint.sh
+++ wp-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-./wait-for-it.sh $WP_DB_HOST:$WP_DB_PORT -- mysql -h $WP_DB_HOST -u $WP_DB_USER -p$WP_DB_PASS $WP_DB_NAME < db_dump/penkit-wp-$WP_VERSION.sql
+./wait-for-it.sh -t 45 $WP_DB_HOST:$WP_DB_PORT -- mysql -h $WP_DB_HOST -u $WP_DB_USER -p$WP_DB_PASS $WP_DB_NAME < db_dump/penkit-wp-$WP_VERSION.sql
 source /etc/apache2/envvars
 apache2 -DFOREGROUND
